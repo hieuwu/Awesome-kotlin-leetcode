43374e6dee6964d9f9d86bbcb6b7e6c6875bb523
hieuvu99 <star.hieu.99@gmail.com>
2020-10-06 20:09:38 +0700

    remove: unnecessary file

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,8 @@
-fun main() {
-        var head = ListNode()
-        var pFinder = head
-        //add a value to list
-        //create node with a value
-        var aNode = ListNode(5)
-        var aNode2 = ListNode(6)
-        //Traverse over the list, last node.next -> newNode
-        var lastNode = head.goToLastNodeFromHead(head)
-        lastNode.next = aNode
-        lastNode = head.goToLastNodeFromHead(head)
-        lastNode.next = aNode2
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.math.pow
 
-        while (pFinder.next != null) {
-                println("value: ${pFinder.value}")
-                pFinder = pFinder.next!!
-        }
-        print("Done")
+fun main() {
+
+        print("Done " )
 }
