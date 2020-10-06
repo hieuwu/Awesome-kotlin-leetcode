09aabaae0ecede6ac9983fa738d40461e4579981
hieuvu99 <star.hieu.99@gmail.com>
2020-10-06 20:27:47 +0700

    add: solution for remove node at N from end

--- src/RemoveNNodeFromEnd.kt
+++ src/RemoveNNodeFromEnd.kt
@@ -1,2 +1,34 @@
 class RemoveNNodeFromEnd {
+    companion object {
+        fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+            if (nodeCount(head) <= 1 && n ==1) {
+                return null
+            }
+            var headCLone:ListNode? = head
+            var p = head
+            var i = nodeCount(head) - n
+            if (i==0) {
+                var newHead: ListNode? = head!!.next
+                return newHead
+            }
+            println("Index: " + i)
+            var ite  = 0
+            while(p!!.next!= null && ite < i - 1) {
+                ite++
+                p = p.next
+            }
+            p.next = p.next!!.next
+            return head
+        }
+
+        fun nodeCount(head: ListNode?): Int {
+            var p = head
+            var i: Int = 1
+            while((p!!.next) != null) {
+                i++
+                p = p.next
+            }
+            return i
+        }
+    }
 }
