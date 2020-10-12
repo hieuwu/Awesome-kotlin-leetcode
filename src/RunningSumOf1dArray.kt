092a1c8c130b96353f157fafe0c03c28728e6efe
hieuvu99 <star.hieu.99@gmail.com>
2020-10-12 21:58:29 +0700

    add: solution for RunningSumOf1dArray

--- src/RunningSumOf1dArray.kt
+++ src/RunningSumOf1dArray.kt
@@ -1,2 +1,13 @@
 class RunningSumOf1dArray {
+    companion object {
+        fun runningSum(nums: IntArray): IntArray {
+            var result: ArrayList<Int> = ArrayList<Int>()
+            var sum  = 0
+            for(i in 0.. nums.size-1) {
+                sum += nums[i]
+                result.add(sum)
+            }
+            return result.toIntArray()
+        }
+    }
 }
