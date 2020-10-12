e8f5e65be70a53e06e74c5183751eef70c5fc069
hieuvu99 <star.hieu.99@gmail.com>
2020-10-12 21:59:21 +0700

    add: solution for valindrome linkedlist

--- src/ValindromeLinkedList.kt
+++ src/ValindromeLinkedList.kt
@@ -1,2 +1,25 @@
 class ValindromeLinkedList {
+    companion object {
+        fun isPalindrome(head: ListNode?): Boolean {
+            if (head == null) {
+                return true
+            }
+            var p = head
+            var arr:ArrayList<Int>  = ArrayList<Int>()
+            while(p!=null) {
+                arr.add(p!!.`val`!!)
+                p = p?.next
+            }
+            var i =0
+            var n = arr.size
+            while (i<=arr.size/2) {
+                if (arr.get(i) != arr.get(n-i-1)) {
+                    return false
+                }
+                i++
+            }
+            print(arr)
+            return true
+        }
+    }
 }
