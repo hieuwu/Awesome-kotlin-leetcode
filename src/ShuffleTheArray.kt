a621c69456af4ca99aca55011327ca62d82bb40a
hieuvu99 <star.hieu.99@gmail.com>
2020-10-12 21:58:49 +0700

    add: solution for  ShuffleTheArray

--- src/ShuffleTheArray.kt
+++ src/ShuffleTheArray.kt
@@ -1,2 +1,12 @@
 class ShuffleTheArray {
+    companion object {
+        fun shuffle(nums: IntArray, n: Int): IntArray {
+            var result: ArrayList<Int> = ArrayList<Int> ()
+            for (i in 0..n-1) {
+                result.add(nums[i])
+                result.add(nums[n+i])
+            }
+            return result.toIntArray()
+        }
+    }
 }
