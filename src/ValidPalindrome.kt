4cd5bb2b914ef561a475c720a1f2e83f9b9b03f9
hieuvu99 <star.hieu.99@gmail.com>
2020-10-12 21:59:06 +0700

    add: solution for valid palindrome

--- src/ValidPalindrome.kt
+++ src/ValidPalindrome.kt
@@ -1,2 +1,18 @@
 class ValidPalindrome {
+    companion object {
+        fun isPalindrome(s: String): Boolean {
+            if (s.isEmpty()) {
+                return true
+            }
+            var temp = s.replace("[^A-Za-z0-9]".toRegex(), "").toLowerCase()
+            var n = temp.length
+            for (i in 0..n-1) {
+                if (temp[i] != temp[n-i-1]) {
+                    return false
+                }
+            }
+            print(temp)
+            return true
+        }
+    }
 }
